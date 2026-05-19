69877aa76ba0a788544df4f78f1708b6517d0f10
Shivam Dan <114812318+shivamdan@users.noreply.github.com>
2026-05-19 14:54:35 +0000

    Entering various data types

--- basics_01/02_dataTypes.js
+++ basics_01/02_dataTypes.js
@@ -1,2 +1,21 @@
 // "use strict", when I type this phrase written in double quotes on top of a js file then it uses the syntax of modern javascript.
 
+//alert(3+3) This alert method belongs to the browser environment , and hence it won't work here in node environment. If we try to use this alert here, it will throw error
+
+// Code should be simple and readable
+//Example :-
+console.log(3+3); console.log("Shivam") // Here, both print statements are written in the same line, they should be written in different lines as a proper coding practice
+
+//ECMA Script organization defines the standards in the JS language. Link : tc39.es/ecma262
+
+// Various data types in js
+let name = "Shivam"; // String data type
+let age = 22 // number data type
+let isLoggedIn = false; // boolean data type
+
+// Number => 2^53 on both +ve and -ve sides
+// bigInt => For numbers larger than "Number" range
+// string => "" or ''
+// boolean => true or false
+// null => it's a standalone value(Empty value)
+// undefined => 
