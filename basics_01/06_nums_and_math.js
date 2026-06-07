dbfdba91f045a2a2ee91ae350d821ec5a3be5470
Shivam Dan <114812318+shivamdan@users.noreply.github.com>
2026-06-07 14:07:44 +0000

    Learnt some Math library methods

--- basics_01/06_nums_and_math.js
+++ basics_01/06_nums_and_math.js
@@ -15,3 +15,21 @@ const hundreds = 1000000
 // console.log(hundreds.toLocaleString());
 // console.log(hundreds.toLocaleString('en-IN'));// toLocaleString method is the method, where it converts a number to a string wherein it also put commas to format the number system.
 // By default it is in the us system where after every 3 zeroes there is a comma, but, as you can see in the above , if explicitly mentioned in the argument, like here, it is mentioned to be in the indian system, then , it would be in Indian Decimal system format
+
+// ==================================Math Library===================================
+// To see how many methods are there in the Math library, either go to MDN docs, or go to a new tab in any browser -> inspect -> console -> Type 'Math' -> all methods will be shown
+console.log(Math)
+console.log(Math.abs(-4)) // Gives the absolute value of a number
+console.log(Math.round(4.67))// rounds to the nearest integer
+console.log(Math.floor(4.7)) // Gives the integer part of the number
+console.log(Math.ceil(4.2)) // GIves the next greater integer
+console.log(Math.min(1,2,3,0,-1))// returns minimum of all the real numbers passed in the arguments
+console.log(Math.max(7,2,5,9.9,0))// returns the max of all the real numbers passed in the arguments
+console.log(Math.random());// generates a random real number between 0 and 1
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10;
+const max = 30;
+// A simple way to get a random number between 2 user defined numbers
+console.log(Math.random()*(max-min)+min);
